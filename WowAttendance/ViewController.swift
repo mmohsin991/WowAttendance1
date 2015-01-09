da6ff10e7ec81b68f2654e9ee967ff8570c444a3
mmohsin991 <muhammad.mohsin.991@gmail.com>
2015-01-10 01:29:01 +0500

    Initial Commit

---
+++ WowAttendance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WowAttendance
+//
+//  Created by Mohsin on 10/01/2015.
+//  Copyright (c) 2015 PanaCloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
