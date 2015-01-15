bfd4f0b9d9e5493253528be4c756a2e3a820785a
mmohsin991 <muhammad.mohsin.991@gmail.com>
2015-01-15 22:56:19 +0500

    add login testcase

--- WowAttendanceTests/ModelTests.swift
+++ WowAttendanceTests/ModelTests.swift
@@ -13,12 +13,15 @@ import XCTest
 
 class ModelTests: XCTestCase {
    
-    func testSignInAndUserExist(){
+    func testSignIn(){
         
         // Declare our expectation
-        let loginExpectation = expectationWithDescription("login")
-        let UserIsExistExpectation = expectationWithDescription("login1")
-        
+        let loginExpectation1 = expectationWithDescription("login with correct username and password")
+        let loginExpectation2 = expectationWithDescription("login with correct username and Wrong password")
+        let loginExpectation3 = expectationWithDescription("login with wrong username and correct password")
+        let loginExpectation4 = expectationWithDescription("login with wrong username and wrong password")
+       
+        //if login with correct username and password
         // Call the asynchronous method with completion handler
         wowref.asyncLoginUser("ziaukhan@hotmail.com", password: "123") { (error, user) -> Void in
             
@@ -26,17 +29,57 @@ class ModelTests: XCTestCase {
             XCTAssertNil(error, "login Sussessfuly \(user?.userName)")
             
             // And fulfill the expectation...
-            loginExpectation.fulfill()
+            loginExpectation1.fulfill()
+            
+        }
+        
+        //if login with correct username and Wrong password
+        wowref.asyncLoginUser("ziaukhan@hotmail.com", password: "xyz") { (error, user) -> Void in
+
+            XCTAssertNotNil(error, "login Sussessfuly \(user?.userName)")
+            
+            loginExpectation2.fulfill()
+            
+        }
+        
+        //if login with wrong username and correct password
+        wowref.asyncLoginUser("xyz@hotmail.com", password: "123") { (error, user) -> Void in
+            
+            XCTAssertNotNil(error, "login Sussessfuly \(user?.userName)")
+            
+            loginExpectation3.fulfill()
+            
+        }
+        
+        //if login with wrong username and wrong password
+        wowref.asyncLoginUser("xyz@hotmail.com", password: "xyz") { (error, user) -> Void in
+            
+            XCTAssertNotNil(error, "login Sussessfuly \(user?.userName)")
+            
+            loginExpectation4.fulfill()
             
         }
         
         
+        
+        
+        // Loop until the expectation is fulfilled
+        waitForExpectationsWithTimeout(15, { error in
+            XCTAssertNil(error, "Error")
+        })
+    }
+    
+    func testUserExist(){
+        
+        // Declare our expectation
+        let UserIsExistExpectation = expectationWithDescription("login1")
+
         // Call the asynchronous method with completion handler
         wowref.asynUserIsExist("mmohsin", callBack: { (isExist) -> Void in
             
             // Perform our tests...
             XCTAssertTrue(isExist, "user id exist")
-        
+            
             // And fulfill the expectation...
             UserIsExistExpectation.fulfill()
             
@@ -49,10 +92,5 @@ class ModelTests: XCTestCase {
         })
     }
     
-    func testAdd(){
-        let add = wowref.tempAdd(2, b: 8)
-        
-        XCTAssertEqual(add, 10, "both are equal")
-    }
     
 }
