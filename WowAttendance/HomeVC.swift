20cf0f16164f4a2f4d493142c660920fe94c670f
mmohsin991 <muhammad.mohsin.991@gmail.com>
2015-01-23 02:12:18 +0500

    Merge branch 'NewTheme'
    Conflicts:
    
    WowAttendance.xcodeproj/xcuserdata/mohsin.xcuserdatad/xcdebugger/Breakpo
    ints_v2.xcbkptlist
    WowAttendance/Base.lproj/Main.storyboard

--- WowAttendance/HomeVC.swift
+++ WowAttendance/HomeVC.swift
@@ -20,6 +20,7 @@ class HomeVC: WowUIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var imgBackground: UIImageView!
     
+    @IBOutlet weak var lblUID: UILabel!
     @IBOutlet weak var lblName: UILabel!
     @IBOutlet weak var lblEmail: UILabel!
     @IBOutlet weak var btnAdd: UIButton!
@@ -27,10 +28,11 @@ class HomeVC: WowUIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var loadingInd: UIActivityIndicatorView!
     @IBOutlet weak var loadingLbl: UIButton!
+    @IBOutlet weak var imgUser: UIImageView!
     
     
-    var ownersList = [String: [String: String]]()
-    var subscriber = [String: Int]()
+    var ownersList = [String: [NSObject : AnyObject] ]()
+    var subscriberList = [String: [NSObject : AnyObject] ]()
 
     
     override func viewDidLoad() {
@@ -47,29 +49,74 @@ class HomeVC: WowUIViewController, UITableViewDataSource, UITableViewDelegate {
         self.navigationController?.navigationBar.backgroundColor = UIColor.greenColor()
         self.navigationItem.titleView = imgBarLogo
         
-        println("HomeVCload")
+        // mate user image in round shape
+        self.imgUser.layer.cornerRadius = self.imgUser.frame.size.width/2
+        self.imgUser.layer.masksToBounds = true
         
-        if loginUser != nil{
-            lblName.text = "\(loginUser!.firstName) \(loginUser!.lastName)"
-            lblEmail.text = loginUser?.email
-            
-        }
         
       //  delegate?.collapseSidePanels!()
         
-        // load the orgs and subscriber list
-        wowref.asyncGetUsesOwnerOrgsList("zia", callBack: { (ownersList) -> Void in
-            if ownersList != nil {
-                self.ownersList = ownersList!
-                self.tableView.reloadData()
-                
-                // stop and hide the loading indicators 
-                self.loadingInd.stopAnimating()
-                self.loadingLbl.hidden = true
-            }
-        })
-    }
+//        // load the orgs list
+//         wowref.asyncGetUsesOwnerMemberOrgsList("zia", callBack: { (ownersList, memberList) -> Void in
+//            if ownersList != nil {
+//                self.ownersList = ownersList!
+//                self.tableView.reloadData()
+//                
+//                println(memberList)
+//                
+//                // stop and hide the loading indicators
+//                self.loadingInd.stopAnimating()
+//                self.loadingLbl.hidden = true
+//                
+//            }
+//            if memberList != nil {
+//                
+//                wowref.asynGetOrgsById(["org1", "org2","sda"], callBack: { (orgList) -> Void in
+//                    println(orgList!)
+//                })
+//            }
+//
+//         })
+        
     
+        if loginUser == nil {
+            loginUser = User(ref: "", uID: "shezi", email: "shahzadscs@gmail.com", firstName: "Shahzad", lastName: "Soomro", status: "pending")
+        }
+            loginUser?.asynGetSubscriberOrgs({ (orgList) -> Void in
+                if orgList != nil {
+                    
+                    self.subscriberList = orgList!
+                    self.tableView.reloadData()
+                    
+                    // stop and hide the loading indicators
+                    self.loadingInd.stopAnimating()
+                    self.loadingLbl.hidden = true
+                }
+                
+            })
+            
+            loginUser?.asynGetOwnerOrgs({ (orgList) -> Void in
+                
+                if orgList != nil {
+                    self.ownersList = orgList!
+                    self.tableView.reloadData()
+                    
+                    // stop and hide the loading indicators
+                    self.loadingInd.stopAnimating()
+                    self.loadingLbl.hidden = true
+                    
+                }
+                
+            })
+
+    
+        if loginUser != nil{
+            lblUID.text = "@\(loginUser!.uID)"
+            lblName.text = "\(loginUser!.firstName) \(loginUser!.lastName)"
+            lblEmail.text = loginUser!.email
+        }
+    }
+
     override func viewWillAppear(animated: Bool) {
      //   self.imgBackground.image = backgroundImage
     }
@@ -91,12 +138,21 @@ class HomeVC: WowUIViewController, UITableViewDataSource, UITableViewDelegate {
         
         //select subscriber segment
         else if self.segmentControl.selectedSegmentIndex == 1 {
-            return self.subscriber.keys.array.count
+            return self.subscriberList.keys.array.count
         }
         
         return 0
     }
     
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if segmentControl.selectedSegmentIndex == 0 {
+            
+            performSegueWithIdentifier("teamSeg", sender: self.ownersList.keys.array[indexPath.row])
+        }
+        else if segmentControl.selectedSegmentIndex == 1 {
+            performSegueWithIdentifier("teamSeg", sender: self.subscriberList.keys.array[indexPath.row])
+        }
+    }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         
@@ -104,19 +160,23 @@ class HomeVC: WowUIViewController, UITableViewDataSource, UITableViewDelegate {
         
         //select orgs segment
         if self.segmentControl.selectedSegmentIndex == 0 {
-            cell.textLabel?.text = self.ownersList.values.array[indexPath.row]["title"]
-            cell.detailTextLabel?.text = self.ownersList.values.array[indexPath.row]["desc"]
+            cell.textLabel?.text = self.ownersList.values.array[indexPath.row]["title"] as NSString
+            cell.detailTextLabel?.text = self.ownersList.values.array[indexPath.row]["desc"] as NSString
 
         }
             
             //select subscriber segment
         else if self.segmentControl.selectedSegmentIndex == 1 {
-            cell.textLabel?.text = self.subscriber.keys.array[indexPath.row]
-            cell.detailTextLabel?.text = self.ownersList.values.array[indexPath.row].description
+            cell.textLabel?.text = self.subscriberList.values.array[indexPath.row]["title"] as NSString
+            cell.detailTextLabel?.text = self.subscriberList.values.array[indexPath.row]["desc"] as NSString
         }
         
         cell.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.0)
         cell.separatorInset = UIEdgeInsets(top: 0.0, left: 15.0, bottom: 0.0, right: 15.0)
+        
+        cell.imageView?.layer.cornerRadius = 25
+        cell.imageView?.layer.masksToBounds = true
+        
 
         return cell
     }
@@ -124,6 +184,19 @@ class HomeVC: WowUIViewController, UITableViewDataSource, UITableViewDelegate {
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 50.0
     }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "teamSeg" {
+            
+            let desVC = segue.destinationViewController as TeamVC
+            
+            desVC.selectedOrgId = sender as String
+            desVC.memberTypeWithOrg = self.segmentControl.selectedSegmentIndex
+            desVC.delegate = self.delegate
+        }
+    }
+    
     
 
     @IBAction func rightMenu(sender: AnyObject) {
@@ -133,12 +206,12 @@ class HomeVC: WowUIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBAction func segmentControl(segment: UISegmentedControl) {
         if segment.selectedSegmentIndex == 0 {
             self.tableView.hidden = false
-            self.btnAdd.hidden = false
+            self.btnAdd.setTitle("Add Org", forState: UIControlState.Normal)
             self.tableView.reloadData()
         }
         else if segment.selectedSegmentIndex == 1 {
             self.tableView.hidden = false
-            self.btnAdd.hidden = true
+            self.btnAdd.setTitle("Subscribe Org", forState: UIControlState.Normal)
             self.tableView.reloadData()
         }
         
@@ -147,6 +220,7 @@ class HomeVC: WowUIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBAction func addTeam(sender: AnyObject) {
         performSegueWithIdentifier("addTeamSeg", sender: self)
     }
+    
     
 
 }
---
+++ WowAttendance/SubSubTeamVC.swift
@@ -0,0 +1,233 @@
+//
+//  SubSubTeamVC.swift
+//  WowAttendance
+//
+//  Created by Mohsin on 23/01/2015.
+//  Copyright (c) 2015 PanaCloud. All rights reserved.
+//
+
+import UIKit
+
+class SubSubTeamVC: WowUIViewController, UITableViewDataSource, UITableViewDelegate  {
+    
+    @IBOutlet weak var imgBackground: UIImageView!
+    
+    @IBOutlet weak var lblOrgID: UILabel!
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblDesc: UILabel!
+    @IBOutlet weak var btnAdd: UIButton!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var loadingInd: UIActivityIndicatorView!
+    @IBOutlet weak var loadingLbl: UIButton!
+    @IBOutlet weak var imgUser: UIImageView!
+    
+    
+    var teamList = Dictionary<NSObject , AnyObject>()
+    var memberList = Dictionary<NSObject , AnyObject>()
+    
+    // value set by previous
+    var selectedOrgId = String()
+    var selectedTeamId = String()
+    var selectedSubTeamId = String()
+    var memberTypeWithOrg : Int!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        println(selectedTeamId)
+        
+        //segmrnted control default selection
+        self.segmentControl.setEnabled(true, forSegmentAtIndex: 0)
+        
+        
+        // table view configurations
+        self.tableView.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.0)
+        self.tableView.separatorInset = UIEdgeInsets(top: 0.0, left: 15.0, bottom: 0.0, right: 15.0)
+        
+        self.navigationController?.navigationBar.backgroundColor = UIColor.greenColor()
+        self.navigationItem.titleView = imgBarLogo
+        
+        // mate user image in round shape
+        self.imgUser.layer.cornerRadius = self.imgUser.frame.size.width/2
+        self.imgUser.layer.masksToBounds = true
+        
+        
+        println(selectedTeamId)
+        
+        if loginUser != nil {
+            
+            // if user's owner org clicked
+            if self.memberTypeWithOrg == 0 {
+                loginUser?.asynGetOwnerOrgs({ (orgList) -> Void in
+                    
+                    if orgList != nil {
+                        
+                        let tempOrgs :  [String: [NSObject : AnyObject] ] = orgList!
+                        let selectedOrg = tempOrgs[self.selectedOrgId]!
+                        
+                        
+                        let tempTeamsList = (selectedOrg["teams"] as NSDictionary) as Dictionary
+                        let tempSelectedTeam = (tempTeamsList[self.selectedTeamId] as NSDictionary) as Dictionary
+                        
+                        let tempSubTeamsList = (tempSelectedTeam["subteams"] as NSDictionary) as Dictionary
+                        let tempSelectedSubTeam = (tempSubTeamsList[self.selectedSubTeamId] as NSDictionary) as Dictionary
+
+                        // set the name, title and desc of selected org
+                        let name = tempSelectedSubTeam["title"] as NSString
+                        let desc = tempSelectedSubTeam["desc"] as NSString
+                        self.setDesc("@\(self.selectedSubTeamId)", name: name, desc: desc)
+                        
+                        if tempSelectedSubTeam["subteams"] != nil {
+                            self.teamList = (tempSelectedTeam["subteams"] as NSDictionary) as Dictionary
+                        }
+                        
+                        if tempSelectedSubTeam["members"] != nil{
+                            self.memberList = (tempSelectedTeam["members"] as NSDictionary) as Dictionary
+                        }
+                        
+                        
+                        self.tableView.reloadData()
+                        
+                        // stop and hide the loading indicators
+                        self.loadingInd.stopAnimating()
+                        self.loadingLbl.hidden = true
+                        
+                    }
+                    
+                })
+            }
+                
+                // if user's subscriber org clicked
+            else if self.memberTypeWithOrg == 1 {
+                loginUser?.asynGetSubscriberOrgs({ (orgList) -> Void in
+                    if orgList != nil {
+                        
+                        let tempOrgs :  [String: [NSObject : AnyObject] ] = orgList!
+                        let selectedOrg = tempOrgs[self.selectedOrgId]!
+                        
+                        
+                        let tempTeamsList = (selectedOrg["teams"] as NSDictionary) as Dictionary
+                        let tempSelectedTeam = (tempTeamsList[self.selectedTeamId] as NSDictionary) as Dictionary
+                        
+                        let tempSubTeamsList = (tempSelectedTeam["subteams"] as NSDictionary) as Dictionary
+                        let tempSelectedSubTeam = (tempSubTeamsList[self.selectedSubTeamId] as NSDictionary) as Dictionary
+                        
+                        // set the name, title and desc of selected org
+                        let name = tempSelectedSubTeam["title"] as NSString
+                        let desc = tempSelectedSubTeam["desc"] as NSString
+                        self.setDesc("@\(self.selectedSubTeamId)", name: name, desc: desc)
+                        
+                        if tempSelectedSubTeam["subteams"] != nil {
+                            self.teamList = (tempSelectedTeam["subteams"] as NSDictionary) as Dictionary
+                        }
+                        
+                        if tempSelectedSubTeam["members"] != nil{
+                            self.memberList = (tempSelectedTeam["members"] as NSDictionary) as Dictionary
+                        }
+                        
+                        
+                        self.tableView.reloadData()
+                        
+                        // stop and hide the loading indicators
+                        self.loadingInd.stopAnimating()
+                        self.loadingLbl.hidden = true
+                        
+                    }
+                })
+            }
+            
+            
+        }
+        
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        //select teams segment
+        if self.segmentControl.selectedSegmentIndex == 0 {
+            return self.memberList.keys.array.count
+        }
+            
+            //select members segment
+        else if self.segmentControl.selectedSegmentIndex == 1 {
+            return self.teamList.keys.array.count
+            
+        }
+        
+        return 0
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        
+        //select teams segment
+        if self.segmentControl.selectedSegmentIndex == 0 {
+            cell.textLabel?.text = self.memberList.keys.array[indexPath.row] as NSString
+            //
+            //            let firstName = self.memberList.values.array[indexPath.row]["firstName"] as NSString
+            //            let lastName = self.memberList.values.array[indexPath.row]["lastName"] as NSString
+            //            cell.detailTextLabel?.text = "\(firstName) \(lastName)"
+        }
+            
+            //select members segment
+        else if self.segmentControl.selectedSegmentIndex == 1 {
+            cell.textLabel?.text = self.teamList.values.array[indexPath.row]["title"] as NSString
+            cell.detailTextLabel?.text = self.teamList.values.array[indexPath.row]["desc"] as NSString
+        }
+        
+        cell.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.0)
+        cell.separatorInset = UIEdgeInsets(top: 0.0, left: 15.0, bottom: 0.0, right: 15.0)
+        
+        cell.imageView?.layer.cornerRadius = 25
+        cell.imageView?.layer.masksToBounds = true
+        
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 50.0
+    }
+    
+    
+    func setDesc (orgID: String, name: String, desc: String) {
+        lblOrgID.text = orgID
+        lblName.text = name
+        lblDesc.text = desc
+    }
+    
+    @IBAction func rightMenu(sender: AnyObject) {
+        delegate?.toggleRightPanel!()
+    }
+    
+    @IBAction func segmentControl(segment: UISegmentedControl) {
+        if segment.selectedSegmentIndex == 0 {
+            self.tableView.hidden = false
+            self.btnAdd.hidden = false
+            self.tableView.reloadData()
+        }
+        else if segment.selectedSegmentIndex == 1 {
+            self.tableView.hidden = false
+            self.btnAdd.hidden = true
+            self.tableView.reloadData()
+        }
+        
+    }
+    
+    @IBAction func addTeam(sender: AnyObject) {
+        performSegueWithIdentifier("addTeamSeg", sender: self)
+    }
+    
+    
+}
